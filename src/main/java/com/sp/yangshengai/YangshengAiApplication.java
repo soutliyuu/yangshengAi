883ec56bf1ee296b6b04b4885c65a34a3607e6ee
liyuu <2497020624@qq.com>
2025-03-03 18:47:25 +0800

    1

---
+++ src/main/java/com/sp/yangshengai/YangshengAiApplication.java
@@ -0,0 +1,13 @@
+package com.sp.yangshengai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangshengAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangshengAiApplication.class, args);
+    }
+
+}
